6691346c088004aae4c845763ceda30dfc1de4ec
FFellEmoN <102874106+FFellEmoN@users.noreply.github.com>
2023-08-12 00:22:59 +0300

    HW 10 variables
    ДЗ создать 10 переменных

---
+++ TenVariables/TenVariables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte traficLightColor;
+            sbyte rangeCrosswalk;
+            short age;
+            ushort timeBeforeBirth;
+            int time;
+            uint operationalTime;
+            long value;
+            ulong number;
+            char symbolOfTeam;
+            bool IsOpenDoor;
+        }
+    }
+}
