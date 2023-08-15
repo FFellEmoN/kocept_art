a25791f4b629096c2469f923f19bd5717d9a68ca
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-08-16 00:40:34 +0300

    ДЗ работа со строками

---
+++ WorkingWithStrings/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string nationality;
+            string profession;
+            string zodiacSign;
+            int age;
+
+            Console.WriteLine("Введите ваше имя ?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Введите ваш возраст ?");
+            age = Convert.ToInt32( Console.ReadLine() );
+
+            Console.WriteLine("Введите вашу национальность ?");
+            nationality = Console.ReadLine();
+
+            Console.WriteLine("Введите вашу профессию ?");
+            profession = Console.ReadLine();
+
+            Console.WriteLine("Какой у вас знак задиака ?");
+            zodiacSign = Console.ReadLine();
+
+            Console.WriteLine($"Ваше имя {name}, " +
+                $"ваш возраст {age}, " +
+                $"ваша национальность {nationality}, " +
+                $"ваша профессия {profession}," +
+                $"ваш знак зодиака {zodiacSign}");
+        }
+    }
+}
