42238838396570e63e4b9ed41dfe98c4f7ea264c
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-08-17 00:23:55 +0300

    ДЗ с картинками
    Количество высчитывается делением,
    а остаток делением с остатком

---
+++ Pictures/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Album = 52;
+            int numberOfPhotos = 3;
+            int numberOfRows;
+            int remains;
+
+            numberOfRows = Album / numberOfPhotos;
+            remains = Album % numberOfPhotos;
+            Console.WriteLine($"Количество полностью заполненных рядов: {numberOfRows}");
+            Console.WriteLine($"Картинок сверх меры: {remains}");
+        }
+    }
+}
