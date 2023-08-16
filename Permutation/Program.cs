5231fe70c75468c8155bc8bc9151454d19a9a1c1
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-08-17 00:42:20 +0300

    ДЗ перестановка местами значений

---
+++ Permutation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Permutation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            int secondValue = 7;
+            int value;
+
+            Console.WriteLine("До замены");
+            Console.WriteLine($"Первое: {number}, Второе: {secondValue}");
+
+            value = number;
+            number = secondValue;
+            secondValue = value;
+
+            Console.WriteLine("После замены");
+            Console.WriteLine($"Первое: {number}, Второе: {secondValue}");
+        }
+    }
+}
