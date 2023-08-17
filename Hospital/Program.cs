fe7b3a2754a9d5d4230137c8a6e4216b525a5a9d
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-08-17 22:58:28 +0300

    ДЗ Поликлиника

---
+++ Hospital/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople;
+            int hours;
+            int minutes;
+            const int timeForOnePerson = 10;
+            const int minInHours = 60;
+            
+
+            Console.WriteLine("Введите количество человек в очереди.");
+            numberOfPeople = Convert.ToInt32(Console.ReadLine());
+            minutes = numberOfPeople * timeForOnePerson;
+            hours = minutes / minInHours;
+            minutes %= minInHours;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {hours} часа и {minutes} минут.");
+            Console.ReadKey();
+        }
+    }
+}
