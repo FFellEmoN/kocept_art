fd30f56b4c7df044dfe4e6661a078797b4cbd0ce
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-08-17 22:30:59 +0300

    ДЗ магазин кристаллов

---
+++ shopCrystals/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shopCrystals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystals = 123;
+            int characterCrystals = 0;
+            int costCrystal = 10;
+            int desiredNumberOfCrystals;
+
+            Console.WriteLine("Введите количества золота.");
+            gold = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"В магазине имеется {crystals} шт кристалов. " +
+                $"Цена 1 шт = {costCrystal}. " +
+                $"Сколько кристаллов вы хотите купить?");
+            desiredNumberOfCrystals = Convert.ToInt32(Console.ReadLine());
+            gold -= desiredNumberOfCrystals * costCrystal;
+            characterCrystals += desiredNumberOfCrystals;
+
+            Console.WriteLine($"У вас кристалов: {characterCrystals}");
+            Console.WriteLine($"У вас золота: {gold}");
+        }
+    }
+}
