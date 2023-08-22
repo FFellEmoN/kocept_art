0f657ddb6c1d02f3e3766811bcd7318e9f375665
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-08-22 23:24:17 +0300

    ДЗ Освоение циклов

---
+++ JustFor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCycles;
+            string masage = "Привет";
+
+            Console.Write("Введите количество повторений: ");
+            numberOfCycles = Convert.ToInt32(Console.ReadLine());
+           while (numberOfCycles-- > 0)
+            {
+                Console.WriteLine(masage);
+            }
+        }
+    }
+}
