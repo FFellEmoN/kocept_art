6bbd89c63cfe9afc5faaca488f7b9727041ab367
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-08-22 22:34:17 +0300

    Работа над ошибкаим
    1) Исправлен порядок переменных
    2) Испр. пробелы между строк
    3) Испр. название переменных

--- Hospital/Program.cs
+++ Hospital/Program.cs
@@ -10,20 +10,19 @@ namespace Hospital
     {
         static void Main(string[] args)
         {
-            int numberOfPeople;
-            int hours;
-            int minutes;
             const int timeForOnePerson = 10;
             const int minInHours = 60;
-            
+            int numberOfPeople;
+            int hoursInLine;
+            int minutesInLine;
 
             Console.WriteLine("Введите количество человек в очереди.");
             numberOfPeople = Convert.ToInt32(Console.ReadLine());
-            minutes = numberOfPeople * timeForOnePerson;
-            hours = minutes / minInHours;
-            minutes %= minInHours;
-
-            Console.WriteLine($"Вы должны отстоять в очереди {hours} часа и {minutes} минут.");
+            minutesInLine = numberOfPeople * timeForOnePerson;
+            hoursInLine = minutesInLine / minInHours;
+            minutesInLine %= minInHours;
+            Console.WriteLine($"Вы должны отстоять в очереди {hoursInLine} часа и {minutesInLine} минут.");
+            Console.ReadKey();
         }
     }
 }
