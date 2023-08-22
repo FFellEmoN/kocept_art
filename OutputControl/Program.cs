03517bb4b3e3de55aad91e207dc66a0326ffa06d
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-08-22 23:42:01 +0300

    ДЗ Контроль входа

---
+++ OutputControl/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string controlWorld = "exit";
+            string masage;
+
+            Console.Write("Введите проверочное слово: ");
+            masage = Console.ReadLine();
+            while (masage != controlWorld)
+            {
+                Console.WriteLine("Введите новое проверочное слово!");
+                masage = Console.ReadLine();
+            }
+            Console.WriteLine("Проверочное слово верное!");
+        }
+    }
+}
