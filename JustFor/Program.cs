154aec924d0e87e404cf61d3b8964c721b85acc7
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-08-23 22:47:26 +0300

    ДЗ Освоение циклов
    Цикл while заменен на for
    Исправлены пустые строки

--- JustFor/Program.cs
+++ JustFor/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +12,19 @@ namespace JustFor
         static void Main(string[] args)
         {
             int numberOfCycles;
-            string masage = "Привет";
+            string masage;
 
+            Console.Write("Введите сообщение: ");
+            masage = Console.ReadLine();
             Console.Write("Введите количество повторений: ");
             numberOfCycles = Convert.ToInt32(Console.ReadLine());
-           while (numberOfCycles-- > 0)
+
+            for (int i = 0; i < numberOfCycles; i++)
             {
                 Console.WriteLine(masage);
             }
+
+            Console.ReadLine();
         }
     }
 }
