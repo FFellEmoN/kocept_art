68ceb5bdc0a4ceb1f6d06fc1efe37c4b691c57e2
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-08-23 23:40:36 +0300

    ДЗ последовательность

---
+++ Sequence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 5;
+            int valueSteap = 7;
+            int steaps = 14;
+
+            for (int i = 0; i < steaps ; i++)
+            {
+                Console.WriteLine(value);
+                value += valueSteap;
+            }
+            Console.ReadKey();
+        }
+    }
+}
