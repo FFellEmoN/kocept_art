a53146ae717ad1601e7d3750b9d2d68a73772c65
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-09-12 23:02:12 +0300

    ДЗ Вывод имени

---
+++ OutputName/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            char character;
+
+            int additionalCharacters = 3;
+
+            Console.WriteLine("Введите ваше имя.");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите символ для рамки вокруг имени.");
+            character = Convert.ToChar(Console.ReadLine());
+            
+            for (int i = 0; i <= name.Length + additionalCharacters; i++)
+            {
+                Console.Write(character);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(character + " " + name + " " + character);
+
+            for (int i = 0; i <= name.Length + additionalCharacters; i++)
+            {
+                Console.Write(character);
+            }
+        }
+    }
+}
