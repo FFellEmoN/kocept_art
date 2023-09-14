264c821fcaa469963e39f777815972c11d975477
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-09-15 00:32:36 +0300

    ДЗ Степень двойки

---
+++ DegreeTwo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegreeTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int conditionNumber;
+            int number = 2;
+            int startDegree = 1;
+            int degree = startDegree;
+            int result = 1;
+
+            Random random = new Random();
+            conditionNumber = random.Next(11);
+
+            Console.WriteLine($"Случайное число: {conditionNumber}");
+
+            if (conditionNumber >= number)
+            {
+                do
+                {
+                    for (int i = 0; i < degree; i++)
+                    {
+                        result *= number;
+                    }
+
+                    if (conditionNumber >= result)
+                    {
+                        result = 1;
+                        degree++;
+                    }
+                } while (conditionNumber >= result);
+            }
+
+            Console.WriteLine($"Степень: {degree}");
+            Console.WriteLine($"{number} в степени {degree} = {result}");
+        }
+    }
+}
