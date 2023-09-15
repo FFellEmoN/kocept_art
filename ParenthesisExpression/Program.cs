20f5e02288361ab028d59739fcb8d8c2f09df4ca
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-09-15 22:59:51 +0300

    ДЗ Скобочное выражение

---
+++ ParenthesisExpression/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParenthesisExpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lineSymbols = "(())";
+
+            bool isLineCorrect = true;
+
+            int lastSymbolCorrectLineSymbol = lineSymbols.Length - 1;
+            int firstSymbolCorrectLintSymbol = 0;
+            int death = 0;
+            int maxDeath = 0;
+
+            for (int i = 0; i < lineSymbols.Length; i++)
+            {
+                if (lineSymbols[firstSymbolCorrectLintSymbol] == ')' || lineSymbols[lastSymbolCorrectLineSymbol] == '(')
+                {
+                    isLineCorrect = false;
+                }
+
+                if (lineSymbols[i] == '(') 
+                {
+                    death++;
+
+                    if (death > maxDeath)
+                    {
+                        maxDeath = death;
+                    }
+                }
+                else
+                {
+                    death--;
+                }
+            }
+            
+            if (isLineCorrect && death == 0)
+            {
+                Console.WriteLine($"Строка коректная, максимум глубины {maxDeath}"); ;
+            }
+            else
+            {
+                Console.WriteLine($"Строка не коректная");
+            }
+        }
+    }
+}
