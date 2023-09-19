5d46f620dac81668a8b620528ee3f0ba2fa8035f
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-09-19 15:59:39 +0300

    рнш Работа со строками

--- Array/JobWithLine/Program.cs
+++ Array/JobWithLine/Program.cs
@@ -10,37 +10,54 @@ namespace JobWithLine
     {
         static void Main(string[] args)
         {
-            int[,] array = { 
-                { 1, 5, 3 }, 
-                { 6, 7, 2 } 
+            int[,] array = {
+                { 2, 5, 3 },
+                { 6, 7, 2 }
             };
 
+            int firstIndexArray = array[0, 0];
             int numberLine = 1;
             int numberColumn = 0;
             int sumLine = 0;
-            int productColumn = 0;
-        
+            int productColumn = firstIndexArray;
+            int realyNumberLine = numberLine + 1;
+            int realyNumberColumn = numberColumn + 1;
+
+            Console.WriteLine("Массив:");
+
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
                 {
                     Console.Write(array[i, j] + " ");
-
-                    if ( i == numberLine)
-                    {
-                        sumLine += array[i, j];
-                    }
-
-                    if( j == numberColumn)
-                    {
-                        productColumn += array[i, j];
-                    }
                 }
                 Console.WriteLine();
             }
 
-            Console.WriteLine($"Сумма {numberLine + 1} строки: {sumLine}");
-            Console.WriteLine($"Произвидение {numberColumn + 1} столбца: {productColumn}");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++) 
+                {
+                    if (i == numberLine)
+                    {
+                        sumLine += array[i, j];
+                    }
+                }
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (j == numberColumn && array[i, j] != firstIndexArray)
+                    {
+                        productColumn *= array[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Сумма {realyNumberLine} строки: {sumLine}");
+            Console.WriteLine($"Произвидение {realyNumberColumn} столбца: {productColumn}");
         }
     }
 }
