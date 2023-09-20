d665e46dcbe2a15cc3821b56df1f296981d6e181
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-09-21 02:19:15 +0300

    ДЗ Сдвиг значений массива

---
+++ Array/ShiftingArrayValues/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShiftingArrayValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            int firstArrayValue = 0;
+            int numberShifts;
+            int firstIndexArray = 0;
+            int nextIndexArray = 1;
+            int lastIndexArray = array.Length - 1;
+            int requiredNymberIterarions = array.Length - 1;
+
+            Console.WriteLine("Массив до смещения");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.Write("\nВведите количество сдвигов: ");
+            numberShifts = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberShifts; i++)
+            {
+                firstArrayValue = array[firstIndexArray];
+
+                for (int j = 0; j < requiredNymberIterarions; j++)
+                {
+                    array[j] = array[j + nextIndexArray];
+                }
+
+                array[lastIndexArray] = firstArrayValue;
+            }
+
+            Console.WriteLine("Массив после смещения");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
---
+++ Array/ShiftingArrayValues/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ShiftingArrayValues")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShiftingArrayValues")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5a820a84-9b09-48aa-a21e-cc657a287aa9")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
