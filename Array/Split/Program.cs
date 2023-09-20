4f8756b27c6d2e123e510c29efb4004bad5019c2
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-09-21 01:59:23 +0300

    ДЗ Split

---
+++ Array/Split/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Это пример текста, который нужно разделить на слова.";
+
+            string[] words = text.Split(' ');
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
