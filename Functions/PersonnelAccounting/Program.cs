e76ea3c4dbc8d7e96c8a3eb4dae63be5da74bab5
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-09-24 19:00:46 +0300

    дз Кадровый учет

--- Functions/PersonnelAccounting/Program.cs
+++ Functions/PersonnelAccounting/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -37,8 +40,7 @@ namespace PersonnelAccounting
                 switch (disiredOperation)
                 {
                     case 1:
-                        listPersonnels = AddPersonnelsInDossier(listPersonnels);
-                        listPosts = AddPostInDossier(listPosts);
+                        AddDossier(ref listPersonnels, ref listPosts);
                         break;
 
                     case 2:
@@ -46,27 +48,35 @@ namespace PersonnelAccounting
                         break;
 
                     case 3:
-
+                        DeletePersonnelInDossier(ref listPersonnels, ref listPosts);
                         break;
 
                     case 4:
+                        searchSurname(listPersonnels, listPosts);
                         break;
 
                     case 5:
+                        Console.WriteLine("Досвидания!");
                         break;
 
                     default:
                         Console.WriteLine("\nТакой операции не существует\n");
                         break;
                 }
+                Console.ReadKey();
+                Console.Clear();
 
             } while (disiredOperation != ExitMenu);
         }
 
-        private static string[] AddPersonnelsInDossier(string[] listPersonnels)
+        private static void AddDossier(ref string[] listPersonnels, ref string[] listPosts)
         {
-            string[] array = ResizeArray(listPersonnels);
-            int lastIndexArray = array.Length - 1;
+            int valueIncreasingArray = 1;
+
+            listPersonnels = ResizeStringArray(listPersonnels, valueIncreasingArray);
+            listPosts = ResizeStringArray(listPosts, valueIncreasingArray);
+
+            int lastIndexArray = listPosts.Length - 1;
 
             Console.Write("Введите фамилию: ");
             string surname = Console.ReadLine();
@@ -74,47 +84,140 @@ namespace PersonnelAccounting
             string name = Console.ReadLine();
             Console.Write("Введите отчество: ");
             string patronymic = Console.ReadLine();
-
-            string fullName = surname + " " + name + " " + patronymic;
-            array[lastIndexArray] = fullName;
-
-            return array;
-        }
-
-        private static string[] AddPostInDossier(string[] listPosts)
-        {
-            string[] array = ResizeArray(listPosts);
-            int lastIndexArray = array.Length - 1;
-
             Console.Write("Введите должность: ");
             string post = Console.ReadLine();
 
-            array[lastIndexArray] = post;
-
-            return array;
+            string fullName = surname + " " + name + " " + patronymic;
+            listPersonnels[lastIndexArray] = fullName;
+            listPosts[lastIndexArray] = post;
         }
 
-        private static string[] ResizeArray(string[] array)
+        private static string[] ResizeStringArray(string[] array, int value)
         {
-            int valueIncreasingArray = 1;
-            int lengthArray = array.Length + valueIncreasingArray;
+            int lengthArray = array.Length + value;
 
             string[] newArray = new string[lengthArray];
 
-            for (int i = 0; i < array.Length; i++)
+            if (value > 0)
             {
-                newArray[i] = array[i];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    newArray[i] = array[i];
+                }
             }
+
+            if(value < 0)
+            {
+                for (int i = 0; i < newArray.Length; i++)
+                {
+                    newArray[i] = array[i];
+                }
+            }
+
             return newArray;
         }
 
-        private static void WriteAllDossiers(string[] personnels, string[] posts)
+        private static void WriteAllDossiers(string[] listPersonnels, string[] listPosts)
         {
+            if (isArrayEmpty(listPersonnels))
+            {
+                return;
+            }
+
             int indexCorrector = 1;
 
-            for (int i = 0; i < personnels.Length; i++)
+            for (int i = 0; i < listPersonnels.Length; i++)
             {
-                Console.WriteLine(i + indexCorrector + ") " + personnels[i] + " - " + posts[i]);
+                Console.WriteLine(i + indexCorrector + ") " + listPersonnels[i] + " - " + listPosts[i]);
+            }
+        }
+
+        private static void DeletePersonnelInDossier(ref string[] listPersonnels, ref string[] listPosts)
+        {
+            if (isArrayEmpty(listPersonnels))
+            {
+                return;
+            }
+
+            int numberOnListDeleted;
+            int valueIncreasingArray = -1;
+            int indexComponentDeleted;
+            int lastIndexArray = listPersonnels.Length - 1;
+
+            WriteAllDossiers(listPersonnels, listPosts);
+
+            Console.Write("\nКого удалить из списка: ");
+            numberOnListDeleted = Convert.ToInt32(Console.ReadLine());
+
+            if (numberOnListDeleted <= 0 || numberOnListDeleted > listPersonnels.Length)
+            {
+                Console.WriteLine("Некорректный ввод");
+                return;
+            }
+            else if (numberOnListDeleted % 1 != 0)
+            {
+                Console.WriteLine("Некорректный ввод");
+                return;
+            }
+
+            indexComponentDeleted = numberOnListDeleted - 1;
+
+            for (int i = 0; i < listPersonnels.Length; i++)
+            {
+                if (i >= indexComponentDeleted && i != lastIndexArray)
+                {
+                    listPersonnels[i] = listPersonnels[i + 1];
+                    listPosts[i] = listPosts[i + 1];
+                }
+            }
+
+            listPersonnels = ResizeStringArray(listPersonnels, valueIncreasingArray);
+            listPosts = ResizeStringArray(listPosts, valueIncreasingArray);
+        }
+
+        private static void searchSurname(string[] listPersonnels, string[] listPosts)
+        {
+            if (isArrayEmpty(listPersonnels))
+            {
+                return;
+            }
+
+            string surname;
+
+            bool isSurnameFound = false;
+
+            int indexCorrector = 1;
+
+            Console.Write("Введите фамилию: ");
+            surname = Console.ReadLine().ToLower();
+
+            for (int i = 0; i < listPersonnels.Length; i++)
+            {
+                listPersonnels[i].ToLower();
+
+                if (listPersonnels[i].Contains(surname))
+                {
+                    Console.WriteLine($"{i + indexCorrector}) {listPersonnels[i]} - {listPosts[i]}");
+                        isSurnameFound = true;
+                }
+            }
+
+            if (!isSurnameFound)
+            {
+                Console.WriteLine($"{surname} не найден");
+            }
+        }
+
+        private static bool isArrayEmpty(string[] listPersonnels)
+        {
+            if (listPersonnels.Length == 0)
+            {
+                Console.WriteLine("Список пуст!");
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
     }
