b619f5d67052a6f01cf52ca040d2a064bb8bb5b6
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-09-25 22:38:59 +0300

    рнш UIElement

--- Functions/UIElement/Program.cs
+++ Functions/UIElement/Program.cs
@@ -10,29 +10,35 @@ namespace UIElement
     {
         static void Main(string[] args)
         {
-            int health = 5;
-            int maxHealth = 10;
+            float health = 40;
+            float maxHealth = 560;
+
             int positionX = 0;
             int positionY = 0;
-            int procentsHelth = (maxHealth - health) * 10;
+            int sizeBar = 10;
 
             ConsoleColor healthColor = ConsoleColor.Green;
-            ConsoleColor procentsColor = ConsoleColor.Red;
+            ConsoleColor percentsColor = ConsoleColor.Red;
 
-            DrawnBar(health, maxHealth, procentsHelth, procentsColor, healthColor, positionX, positionY);
+            DrawBar(sizeBar, health, maxHealth, percentsColor, healthColor, positionX, positionY);
         }
 
-        private static void DrawnBar(int value, int maxValue, int procent, ConsoleColor procentsColor, ConsoleColor healthColor, int positionX, int positionY)
+        private static void DrawBar(int size, float value, float maxValue, ConsoleColor percentsColor, ConsoleColor healthColor, int positionX, int positionY)
         {
             ConsoleColor defaultColor = Console.BackgroundColor;
             ConsoleColor defaultTextColor = ConsoleColor.White;
 
             string bar = "";
 
-            for (int i = 0; i < value; i++)
-            {
-                bar += " ";
-            }
+            int percentsHelth = Convert.ToInt32(value / maxValue * 100);
+            int startValue = 0;
+            float divisionPrice = maxValue / size;
+
+            //for (int i = 0; i < value; i++)
+            //{
+            //    bar += " ";
+            //}
+            bar = fillingLine(bar, startValue, value, divisionPrice);
 
             Console.SetCursorPosition(positionX, positionY);
             Console.Write('[');
@@ -40,18 +46,34 @@ namespace UIElement
             Console.Write(bar);
             Console.BackgroundColor = defaultColor;
 
-            bar = "";
+            //bar = "";
 
-            for (int i = value; i < maxValue; i++)
-            {
-                bar += " ";
-            }
+            //for (int i = value; i < maxValue; i++)
+            //{
+            //    bar += " ";
+            //}
+            bar = fillingLine(bar, value, maxValue, divisionPrice);
 
             Console.Write(bar + ']');
-            Console.ForegroundColor = procentsColor;
-            Console.Write($" {procent}%");
+            Console.ForegroundColor = percentsColor;
+            Console.Write($" {percentsHelth}%");
             Console.ForegroundColor = defaultTextColor;
             Console.WriteLine();
+        }
+
+        private static string fillingLine(string line, float startValue, float finishValue, float divisionPrice)
+        {
+            line = "";
+
+            finishValue = finishValue / divisionPrice;
+            startValue = startValue / divisionPrice;
+
+            for (float i = startValue; i < finishValue; i++)
+            {
+                line += " ";
+            }
+
+            return line;
         }
     }
 }
