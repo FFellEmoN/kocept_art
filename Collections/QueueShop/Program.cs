0cc43bf29fbb67e4a974c418f58d198ec30a572b
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-10-05 00:51:08 +0300

    ДЗ Очередь в магазине

---
+++ Collections/QueueShop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> buyers = new Queue<int>();
+
+            int checkAmount_1 = 34;
+            int checkAmount_2 = 21;
+            int checkAmount_3 = 134;
+            int checkAmount_4 = 78;
+            int shopAccount = 0;
+
+            buyers.Enqueue(checkAmount_1);
+            buyers.Enqueue(checkAmount_2);
+            buyers.Enqueue(checkAmount_3);
+            buyers.Enqueue(checkAmount_4);
+
+            int lengthQueueBuyers = buyers.Count;
+
+            for (int i = 0; i < lengthQueueBuyers; i++)
+            {
+                Console.WriteLine($"Чек клиента = {buyers.Peek()}");
+                Console.WriteLine("Клиент обслужен!");
+
+                shopAccount += buyers.Dequeue();
+
+                Console.WriteLine($"Денег на счете магазина: {shopAccount}");
+                Console.WriteLine("Нажмите любую клавишу для продолжения обслуживания.");
+                Console.ReadKey();
+
+                Console.Clear();
+            }
+
+            Console.WriteLine("Очередь пуста, вы обслужили всех клиентов!");
+        }
+    }
+}
