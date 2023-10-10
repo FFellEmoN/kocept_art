047fae9d7550538d570fb8251169e20d93a87ac5
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-10-10 15:26:39 +0300

    ДЗ Работа с классами

---
+++ ObjectoCrientedProgramming/WorkClasses/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(1, "SuperMan", "Супер герой с множеством способностей");
+
+            player.ShowInfoPlayer();
+        }
+    }
+
+    class Player
+    {
+        private int id;
+        private string name;
+        private string description;
+
+        public Player(int id, string name, string description)
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+        }
+
+        public void ShowInfoPlayer()
+        {
+            Console.WriteLine("Информа об игроке");
+            Console.WriteLine($"id: {id}\nИмя: {name}\nОписание: {description}.");
+        }
+    }
+}
