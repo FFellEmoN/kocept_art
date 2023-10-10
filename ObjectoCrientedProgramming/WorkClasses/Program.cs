274c616776c6245c7befe9026c0e436797789a99
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-10-10 15:28:26 +0300

    рнш Работа с классами

--- ObjectoCrientedProgramming/WorkClasses/Program.cs
+++ ObjectoCrientedProgramming/WorkClasses/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WorkClasses
 {
@@ -18,21 +14,21 @@ namespace WorkClasses
 
     class Player
     {
-        private int id;
-        private string name;
-        private string description;
+        private int idPlayer;
+        private string namePlayer;
+        private string descriptionPlayer;
 
         public Player(int id, string name, string description)
         {
-            this.id = id;
-            this.name = name;
-            this.description = description;
+            this.idPlayer = id;
+            this.namePlayer = name;
+            this.descriptionPlayer = description;
         }
 
         public void ShowInfoPlayer()
         {
             Console.WriteLine("Информа об игроке");
-            Console.WriteLine($"id: {id}\nИмя: {name}\nОписание: {description}.");
+            Console.WriteLine($"idPlayer: {idPlayer}\nИмя: {namePlayer}\nОписание: {descriptionPlayer}.");
         }
     }
 }
