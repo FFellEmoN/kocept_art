1a3dd89cd44a9c0ce9ffa601820b21f35c7037a4
Maksim <102874106+FFellEmoN@users.noreply.github.com>
2023-11-14 17:57:35 +0300

    рнш Магазин

--- ObjectoCrientedProgramming/Shoop/Program.cs
+++ ObjectoCrientedProgramming/Shoop/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlTypes;
-using System.Runtime.InteropServices;
 
 namespace Shoop
 {
@@ -9,51 +7,114 @@ namespace Shoop
     {
         static void Main(string[] args)
         {
+            const string OpenShoopMenu = "1";
+            const string ShowItemsPlayrMenu = "2";
+            const string ShowSallerMoneyMenu = "3";
+            const string ShowPlayerMoneyMenu = "4";
+            const string ExitMenu = "5";
+
+            string diciredAction;
+            bool IsExit = true;
+
             Player player = new Player();
+
             Saller saller = new Saller();
 
+            Shoop shoop = new Shoop(saller, player);
+            do
+            {
+                Console.WriteLine($"{OpenShoopMenu}) - открыть магазин.");
+                Console.WriteLine($"{ShowItemsPlayrMenu}) - показать предметы игрока.");
+                Console.WriteLine($"{ShowSallerMoneyMenu}) - показать деньги {saller.Name}.");
+                Console.WriteLine($"{ShowPlayerMoneyMenu}) - показать деньги {player.Name}.");
+                Console.WriteLine($"{ExitMenu}) - выйти.");
+
+                Console.Write("\nВведите желаемое действие: ");
+                diciredAction = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (diciredAction)
+                {
+                    case OpenShoopMenu:
+                        shoop.Open();
+                        break;
+
+                    case ShowItemsPlayrMenu:
+                        player.ShowItems();
+                        break;
+
+                    case ShowSallerMoneyMenu:
+                        saller.ShowMoney();
+                        break;
+
+                    case ShowPlayerMoneyMenu:
+                        player.ShowMoney();
+                        break;
+
+                    case ExitMenu:
+                        IsExit = false;
+                        break;
+                }
+
+                Console.WriteLine("Нажмите любую клавишу, чтобы продолжить.");
+                Console.ReadKey();
+                Console.Clear();
+            } while (IsExit);
+        }
+    }
+
+    class Shoop
+    {
+        private Saller _saller;
+        private Player _player;
+        public Shoop(Saller saller, Player player)
+        {
+            _saller = saller;
+            _player = player;
+
+            _saller.AddItem(new Item("меч", 15));
+            _saller.AddItem(new Item("топор", 20));
+        }
+
+        public void Open()
+        {
             string disiredItem;
 
-            saller.AddItem(new Item("меч", 15));
-            saller.AddItem(new Item("топор", 20));
-
-            Console.WriteLine("Деньги игрока: " + player.Money);
-            Console.WriteLine("Деньги торговца: " + saller.Money);
-            Console.WriteLine();
-            Console.WriteLine("Какой товар вы хотите приобрести.");
-            saller.ShowItems();
+            _saller.ShowMoney();
+            _player.ShowMoney();
+            Console.WriteLine("\nКакой товар вы хотите приобрести ?\n");
+            _saller.ShowItems();
 
             Console.Write("\nВведите номер товара:");
             disiredItem = Console.ReadLine();
 
             if (int.TryParse(disiredItem, out int number) &&
                 number > 0 &&
-                number <= saller.GetCountItems())
+                number <= _saller.GetCountItems())
             {
-                saller.SellItem(player, number);
+                if (_player.BuyItem(_saller.GetItem(number)))
+                {
+                    _saller.SellItem(number);
+                }
             }
             else
             {
                 Console.WriteLine("Вы ввели не число или некоректное число.");
             }
-
-            Console.WriteLine("Деньги игрока: " + player.Money);
-            Console.WriteLine("Деньги торговца: " + saller.Money);
-            Console.WriteLine();
-            player.ShowItems();
         }
     }
 
     class Item
     {
-        public string Name { get; private set; }
-        public float Coast { get; private set; }
-
         public Item(string name, float coast)
         {
             Name = name;
             Coast = coast;
         }
+
+        public string Name { get; private set; }
+
+        public float Coast { get; private set; }
 
         public override string ToString()
         {
@@ -63,31 +124,38 @@ namespace Shoop
 
     class TradeCharacter
     {
-        protected List<Item> _items;
+        protected List<Item> Items;
 
         protected TradeCharacter()
         {
-            _items = new List<Item>();
+            Items = new List<Item>();
         }
 
         public float Money { get; protected set; }
 
+        public string Name { get; protected set; }
+
         public int GetCountItems()
         {
-            return _items.Count;
+            return Items.Count;
         }
 
         public void AddItem(Item item)
         {
-            _items.Add(item);
+            Items.Add(item);
         }
 
         public virtual void ShowItems()
         {
-            for (int i = 0; i < _items.Count; i++)
+            for (int i = 0; i < Items.Count; i++)
             {
-                Console.WriteLine($"{i + 1}) {_items[i].ToString()}");
+                Console.WriteLine($"{i + 1}) {Items[i].ToString()}");
             }
+        }
+
+        public void ShowMoney()
+        {
+            Console.WriteLine($"Деньги {Name}: " + Money);
         }
     }
 
@@ -96,16 +164,17 @@ namespace Shoop
         public Saller()
         {
             Money = 300f;
+            Name = "Торговец";
         }
-        public void SellItem(Player player, int number)
-        {
-            int index = number - 1;
 
-            if (_items.Contains(_items[index]) && player.BuyItem(_items[index]))
+        public void SellItem(int number)
+        {
+
+            if (Items.Contains(GetItem(number)))
             {
-                Money += _items[index].Coast;
-                Console.WriteLine($"Продан {_items[index].ToString()}");
-                _items.Remove(_items[index]);
+                Money += GetItem(number).Coast;
+                Console.WriteLine($"Продан {GetItem(number).ToString()}");
+                Items.Remove(GetItem(number));
             }
             else
             {
@@ -118,6 +187,13 @@ namespace Shoop
             Console.WriteLine("Товары для продажи:\n");
             base.ShowItems();
         }
+
+        public Item GetItem(int number)
+        {
+            int index = number - 1;
+
+            return Items[index];
+        }
     }
 
     class Player : TradeCharacter
@@ -125,6 +201,7 @@ namespace Shoop
         public Player()
         {
             Money = 300f;
+            Name = "Игрок";
         }
 
         public bool BuyItem(Item item)
